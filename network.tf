633b08e6b0e9794e2c8952a764de34794b4c34bc
Sideok You <sideokyou@gmail.com>
2022-02-09 20:39:13 +0000

    updated network group name

--- network.tf
+++ network.tf
@@ -2,5 +2,4 @@ module "network" {
   source  = "app.terraform.io/sideok-ucla/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = sideok-ucla.hashicat-azure
-}
+  resource_group_name = "${var.prefix}-workshop"
